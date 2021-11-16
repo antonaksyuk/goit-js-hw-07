8c03748b78cda9494708f39c7c5caa17db2f83f5
antonaksyuk <87242472+antonaksyuk@users.noreply.github.com>
2021-11-16 16:26:45 +0200

    upd

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createGalleryMurkup(items) {
+    return items
+    .map(({ preview, original, description }) => {
+
+       return `
+   <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+        .join('');
+}
+const galleryContainer = document.querySelector('.gallery');
+const galleryMurkup = createGalleryMurkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMurkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+}
+);
